6dbe26a5ae722b6bac806de1888e4b6e25dfae40
The Lazy Guy <103317378+imthelazyguy@users.noreply.github.com>
2025-05-20 12:51:33 +0600

    Update and rename keepalive.js to keepAlive.js

---
+++ src/keepAlive.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('NOVA is alive!');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`🌐 Keep-alive server running on port ${PORT}`);
+});
