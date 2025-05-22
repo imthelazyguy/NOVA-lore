fe6e37150ac1853a6291c0a8d5c1c4f9df933ca7
The Lazy Guy <103317378+imthelazyguy@users.noreply.github.com>
2025-05-22 18:08:14 +0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const { Client, GatewayIntentBits, Collection } = require('discord.js');
 const fs = require('fs');
-//const path = require('path');//
+const path = require('path');
 require('./keepAlive');
 
 // Load Firestore DB
